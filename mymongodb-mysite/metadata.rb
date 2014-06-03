5e2a59b69af25b557e8d1170c0f2bbd8d04069d1
Administrator <stefano.vital@gmail.com>
2014-06-03 23:56:12 +0100

    first commit

---
+++ mymongodb-mysite/metadata.rb
@@ -0,0 +1,9 @@
+name             'mongodb-mysite'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures mongodb-mysite'
+long_description 'Installs/Configures mongodb-mysite'
+version          '0.1.0'
+
+depends 'mongodb'
---
+++ mymongodb-mysite/recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: mymongo
+# Recipe:: default
+#
+# Copyright (C) 2014 YOUR_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe "mongodb::10gen_repo"
+include_recipe "mongodb::default"
