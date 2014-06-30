0f2ae64131f5eb9e00c51f9669e8332ce32d838d
Administrator <stefano.vital@gmail.com>
2014-06-30 20:42:59 +0100

    include_recipe

--- mymongodb-mysite/recipes/default.rb
+++ mymongodb-mysite/recipes/default.rb
@@ -9,7 +9,7 @@
 
 include_recipe "mongodb::10gen_repo"
 include_recipe "mongodb::default"
-include 'aws'
+include_recipe 'aws'
 aws_ebs_volume "mysql_data_volume" do
   provider "aws_ebs_volume"
   volume_id "vol-524dee17"
